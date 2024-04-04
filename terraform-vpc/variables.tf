d442fa6e2c794f98d071e02a7eba0f6e7ca6c963
Pennamahesh <123188673+Pennamahesh@users.noreply.github.com>
2024-04-04 11:53:12 +0530

    Add files via upload
    vpc module,ec2 module

---
+++ terraform-vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+    description = "vpc cidr"
+    type = string
+  
+}
+variable "public_subnet" {
+    description = "public subnet"
+    type=list(string)
+  
+}
+variable "instance_type" {
+    description = "instance_type"
+    type = string
+  
+}
