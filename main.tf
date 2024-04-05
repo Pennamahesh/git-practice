46ceb484084a810de03d3ea141323be234e70896
mahesh <pennamahesh2001@gmail.com>
2024-04-05 11:42:05 +0530

    version-2

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "us-east-1"
+
+}
+resource "aws_instance" "mahesh" {
+    ami = ""
+    instance_type = ""
+  
+}
+resource "aws_s3_bucket" "maheshh" {
+    bucket = "pennmaheshbabu123456789"
+    tags = {
+        Name="mahesh"
+        environment="dev"
+    }
+  
+}
+resource "aws_s3_bucket" "name" {
+  
+}
